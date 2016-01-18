cdc5131cf199b626a081029f78519b90ffd48355
Kiran <kunigirik@yahoo.com>
2016-01-17 16:23:17 -0800

    Created a grid version of my website

--- short/script.js
+++ short/script.js
@@ -7,15 +7,37 @@ $(document).ready(function() {
 
     // SETUP
     $.adaptiveBackground.run();
+    $(".cover").hide();
 
+    var width = $(window).width() / 7;
     $(".box").each(function(index) {
-//        $(this).css("background-color", randomColor());
+        $(this).width(width);
+        $(this).height(width);
     });
 
     TweenMax.set(".boxOverlay", {
         y: $(".box").height()
     });
 
+
+    // Close button
+    $('.xbtn').on("click", function(e) {
+        e.preventDefault();
+        $(".cover").hide();
+        console.log("hello");
+    });
+    
+    $('.boxOverlay').on("click", function(e) {
+        e.preventDefault();
+        var descriptionText = $(this).find('.boxDescription:first').text();
+        var titleText = $(this).find('p:first').text();
+        $(".cover").find('.coverDescription:first').text(descriptionText);
+        $(".cover").find('.coverTitle:first').text(titleText);
+        $(".cover").show();
+        console.log(text);
+    });
+
+    // Actions
     var boxHeight = $(".box").height();
     $(".box").hover(
         function() {
